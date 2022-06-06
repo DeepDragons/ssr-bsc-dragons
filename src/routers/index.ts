a27f82791b2194ea9ea92d97ae6eb5c134734a3c
Rinat <lich666black@gmail.com>
2022-06-06 12:23:46 +0300

    added i18n

--- src/routers/index.ts
+++ src/routers/index.ts
@@ -3,9 +3,21 @@ import { wrap } from 'svelte-spa-router/wrap';
 import { routerGuard } from './guard';
 
 import MainPage from '../pages/Main.svelte';
+import DragonPage from '../pages/Dragon.svelte';
+import FlightsPlacePage from '../pages/FlightsPlace.svelte';
+import StorePage from '../pages/Store.svelte';
 
 export default {
   '/': wrap({
     component: MainPage
+  }),
+  '/dragon/:id': wrap({
+    component: DragonPage
+  }),
+  '/fights': wrap({
+    component: FlightsPlacePage
+  }),
+  '/store': wrap({
+    component: StorePage
   })
 };
