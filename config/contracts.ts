eb1d2870253f609ea0beea8763f0ef90abf04f94
Rinat <lich666black@gmail.com>
2022-06-08 16:15:30 +0300

    fixed contracts

--- config/contracts.ts
+++ config/contracts.ts
@@ -1,13 +1,7 @@
 export enum Contracts {
-  Distributer = `0x066b88d3411c68cb56219e748ae895e1734c0f51`,
-  Main = `0xb4d83becb950c096b001a3d1c7abb10f571ae75f`,
-  FightPlace = `0xf0a3fbcfa48e4c796daafbb9c6341d68ff326b64`,
-  GenLab = `0x8a30485597ebcc1be49e75261de83d30e4a9d1e7`,
-  ZIlPay = `0xfbd07e692543d3064b9cf570b27faabfd7948da4`,
-  Breed = `0xade7886ec4a36cb0a7de2f5d18cc7bdae12e3650`,
-  MarketPlace = `0x7b9b80aaf561ecd4e89ea55d83d59ab7ac01a575`,
-  Wallet = `0x54fb9f7e7b6423677d4ffd67b53d452f6d0fa509`,
-  Necropolis = `0x6eaf9b37f9870994f5853eb28405919f87e8dec2`,
-  Name = `0x0f5d8f74817e2bc5a09521149094a7860c691d42`,
+  Distributer = `0x40095901EC0d52c091b4907E6E998730DA62B5Aa`,
+  Main = `0xDB44F12dff43498e371e470531D7253edbb66652`,
+  Mutagen = '0x0baD725758c48b61cF86153885A43839b2762CD2',
+  FightPlace = `0x03E445603113eACe4Bc2a8B0E2Ab6DbB13eAeAED`,
   NIL = `0x0000000000000000000000000000000000000000`,
 }
--- next.config.js
+++ next.config.js
@@ -24,6 +24,14 @@ const securityHeaders = [
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    // These are all the locales you want to support in
+    // your application
+    locales: ['en-US'],
+    // This is the default locale you want to be used when visiting
+    // a non-locale prefixed path e.g. `/hello`
+    defaultLocale: 'en-US'
+  },
   headers() {
     return [
       {
