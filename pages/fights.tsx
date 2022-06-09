e71cda33e5b1048d9f9772c12f2de8ca3ab6e4ae
Rinat <lich666black@gmail.com>
2022-06-09 11:38:06 +0300

    fixed localses page

--- pages/fights.tsx
+++ pages/fights.tsx
@@ -2,24 +2,20 @@ import type { GetServerSidePropsContext, NextPage } from 'next';
 
 import styles from '../styles/pages/fights.module.scss';
 
-import Head from 'next/head';
-import Image from 'next/image';
-
-import { SkeletCard } from '@/skelets/skelet-card';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 
-const DragonsPage: NextPage = () => {
+
+const FightsPage: NextPage = () => {
   return (
     <div className={styles.container}>
-      <SkeletCard />
     </div>
   )
 };
 
 export const getStaticProps = async (props: GetServerSidePropsContext) => ({
   props: {
-    ...await serverSideTranslations(props.locale || `en`, [`dragons`, `common`]),
+    ...await serverSideTranslations(props.locale || `en`, [`fights`, `common`]),
   },
 });
 
-export default DragonsPage;
+export default FightsPage;
